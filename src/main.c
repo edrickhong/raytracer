694cb2d351de9cb2d47a9e8cd873edfddb0e5c6e
ehzx <edrickhong@gmail.com>
2020-01-05 20:23:03 +0800

    proper clear screen and present in

--- src/main.c
+++ src/main.c
@@ -6,8 +6,17 @@
 
 #include "wwindow.h"
 
-s32 main(s32 argc, const s8** argv) {
+void ClearBackBuffer(WBackBufferContext* buffer) {
+	for (u32 y = 0; y < buffer->height; y++) {
+		for (u32 x = 0; x < buffer->width; x++) {
+			u32* pixel = buffer->pixels + (buffer->width * y) + x;
+			// note: default format is argb
+			*pixel = 0xFFFF0000;
+		}
+	}
+}
 
+s32 main(s32 argc, const s8** argv) {
 	WWindowContext window = WCreateWindow(
 	    "Raytracer",
 	    (WCreateFlags)(W_CREATE_NORESIZE | W_CREATE_BACKEND_XLIB), 0, 0,
@@ -17,16 +26,19 @@ s32 main(s32 argc, const s8** argv) {
 
 	WWindowEvent event = {0};
 
+	ClearBackBuffer(&backbuffer);
+
 	b32 run = 1;
 
 	while (run) {
-		while(WWaitForWindowEvent(&window,&event))
-				{
-			if (event.type == W_EVENT_KBEVENT_KEYDOWN) {
-				run = 0;
-			}	
+		WPresentBackBuffer(&window, &backbuffer);
 
-				}
+		while (WWaitForWindowEvent(&window, &event)) {
+			if (event.type == W_EVENT_KBEVENT_KEYDOWN ||
+			    event.type == W_EVENT_CLOSE) {
+				run = 0;
+			}
+		}
 	}
 
 	return 0;
