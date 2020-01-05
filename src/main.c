13211a6701cd68ee9a98f28fddd9b4c7aeebde6f
ehzx <edrickhong@gmail.com>
2020-01-05 10:25:38 +0800

    open a window. woo

--- src/main.c
+++ src/main.c
@@ -6,31 +6,28 @@
 
 #include "wwindow.h"
 
-s32 main(s32 argc, const s8** argv){
+s32 main(s32 argc, const s8** argv) {
 
-	WWindowContext window = WCreateWindow("Raytracer",
-			(WCreateFlags)(W_CREATE_NORESIZE | W_CREATE_BACKEND_XLIB),
-			0,0,
-			1280,720);
+	WWindowContext window = WCreateWindow(
+	    "Raytracer",
+	    (WCreateFlags)(W_CREATE_NORESIZE | W_CREATE_BACKEND_XLIB), 0, 0,
+	    1280, 720);
 
-	WBackBuffer backbuffer = WCreateBackBuffer(&window);
+	WBackBufferContext backbuffer = WCreateBackBuffer(&window);
 
-	WWindowEvent event = {};
+	WWindowEvent event = {0};
 
-	b32 run = true;
+	b32 run = 1;
 
-	while(true){
-
-		while(WWaitForWindowEvent(&window,&event){
-
-				if(event.type == W_EVENT_KBEVENT_KEYDOWN){
-				run = false;	
-				}	
-
-				}
-
+	while (run) {
+		while(WWaitForWindowEvent(&window,&event))
+				{
+			if (event.type == W_EVENT_KBEVENT_KEYDOWN) {
+				run = 0;
+			}	
 
 				}
+	}
 
-				return 0;
-				}
+	return 0;
+}
